bf951f6fbabb9ee4aa0f3a8d8619779f817b2e57
Michael Flores <mich4el.flores@gmail.com>
2022-05-23 13:54:45 -0300

    Upload exercise 5

---
+++ BASE-DE-DATOS_II/ejer_5/views.sql
@@ -0,0 +1,45 @@
+CREATE VIEW listados_pagos_clientes AS
+SELECT nombre_cliente AS "Nombre cliente",
+    telefono,
+    ciudad,
+    pais,
+    fecha_pago,
+    total,
+    id_transaccion
+FROM cliente
+    NATURAL JOIN pago;
+
+CREATE VIEW listado_pedidos_clientes AS
+SELECT nombre_cliente AS "Nombre cliente",
+    telefono,
+    ciudad,
+    pais,
+    codigo_pedido,
+    fecha_pedido,
+    fecha_esperada,
+    fecha_entrega,
+    sum(cantidad * precio_unidad) as "Precio total"
+FROM cliente
+    NATURAL JOIN pedido
+    NATURAL JOIN detalle_pedido
+    NATURAL JOIN producto
+GROUP BY codigo_pedido;
+
+-- stado de los clientes de la ciudad de Madrid que han hecho pagos
+SELECT *
+FROM listados_pagos_clientes
+WHERE ciudad = "madrid";
+
+-- listado de los clientes que todavía no han recibido su pedido
+SELECT *
+FROM listado_pedidos_clientes
+WHERE fecha_entrega IS NULL;
+
+-- calcular el número de pedidos que se ha efectuado cada uno de los clientes
+SELECT COUNT(*)
+FROM listado_pedidos_clientes
+WHERE fecha_entrega IS NOT NULL;
+
+DROP VIEW listado_pagos_clientes;
+
+DROP VIEW listado_pedidos_clientes;
